0c8c8582a6222916a73dda55232da7a59b07a34a
hudsonrikelme <hudsonrikelme@hotmail.com>
2023-05-30 22:42:23 -0300

    Criação de Diretórios na execução do código

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#define MAX_USUARIOS 5
 
+void criarDiretoriosUsuarios(int quantidade){
+    char primeiraparte[] = "U";
+    char nome[10];
+
+    for (int c = 1; c < quantidade; c++){
+        //armazena no buffer o nome do arquivo
+        sprintf(nome, "%s%d", primeiraparte, c);
+        char comando[50];
+        sprintf(comando, "mkdir %s", nome);
+        system(comando);
+    }
+    
+
+
+
+}
 
 
 int main(int argc, char *argv[]){
@@ -9,5 +26,12 @@ int main(int argc, char *argv[]){
         printf("Informe a quantidade de usuários como argumento!\n");
         return 1;
     }
+
+    int quantidade = atoi(argv[1]);
+    if(quantidade <= 0 || quantidade > MAX_USUARIOS){
+        printf("Quantidade de usuários inválida!\n");
+        return 1;
+    }
+    criarDiretoriosUsuarios(quantidade);
    
 }
