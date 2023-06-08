7adf05236c76e063999d1fdcb0b4d94b7b5c81cb
hudsonrikelme <hudsonrikelme@hotmail.com>
2023-06-08 15:37:29 -0300

    Thread de usuario já sabe os arquivos de cada user

--- main.c
+++ main.c
@@ -5,27 +5,39 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 
 #define MAX_USUARIOS 5
 #define MAX_ARQUIVOS 12
 
+pthread_mutex_t mutex; //Declaração do mutex
+
 void *usuario_thread(void *arg) {
     char *diretorio = (char *)arg;
 
+    
+    //Abre o diretório
     DIR *dir = opendir(diretorio);
     if (dir == NULL) {
-        printf("Erro ao abrir o diretorio %s\n", diretorio);
+        printf("Erro ao abrir o diretorio");
+        free(diretorio); // Libera a memória alocada
         pthread_exit(NULL);
     }
 
     struct dirent *entry;
+    //Lê cada entrada do diretório usando a função readdir
     while ((entry = readdir(dir)) != NULL) {
+        //Se houver a entrada de um arquivo regular
         if (entry->d_type == DT_REG) {
-            printf("%s, Arquivo: %s\n", diretorio, entry->d_name);
+            pthread_mutex_lock(&mutex); //Bloqueia o mutex antes de imprimir
+            printf("[%s] -> %s\n", diretorio, entry->d_name);
+            pthread_mutex_unlock(&mutex); //Desbloqueia o mutex após imprimir
         }
     }
-
+    //Diretorio fechado
     closedir(dir);
+    free(diretorio);
+    pthread_exit(NULL);
     return NULL;
 }
 
@@ -50,21 +62,27 @@ int main(int argc, char *argv[]) {
     }
 
     pthread_t threads[MAX_USUARIOS];
+    char diretorio[4];
+
+
+    //Mutex iniciado
+    pthread_mutex_init(&mutex, NULL);
 
     // Criação das threads dos usuários
     for (int i = 0; i < usuarios; i++) {
-        char diretorio[10];
         sprintf(diretorio, "U%d", i+1);
-        if (pthread_create(&threads[i], NULL, usuario_thread, (void *)diretorio) != 0) {
-            printf("Erro ao criar a thread do usuario %s\n", diretorio);
-            return 1;
-        }
+        char *diretorio_thread = malloc(strlen(diretorio) + 1); //Aloca memória para o nome do diretório
+        strcpy(diretorio_thread, diretorio);
+        pthread_create(&threads[i], NULL, usuario_thread, diretorio_thread);
     }
 
     // Aguarda a finalização das threads dos usuários
     for (int i = 0; i < usuarios; i++) {
         pthread_join(threads[i], NULL);
     }
+
+
+    pthread_mutex_destroy(&mutex); //Destroi o mutex
 
     return 0;
 }
