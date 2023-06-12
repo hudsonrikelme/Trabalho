9509b9f9d2b91c24517d86c42bf01bbf272ca6a3
hudsonrikelme <hudsonrikelme@hotmail.com>
2023-06-11 23:12:39 -0300

    Usuário ainda não sabe quais os arquivos q não tem

--- main.c
+++ main.c
@@ -9,40 +9,85 @@
 
 #define MAX_USUARIOS 5
 #define MAX_ARQUIVOS 12
+#define MAX_NOME_ARQUIVO 256
 
-pthread_mutex_t mutex; //Declaração do mutex
+pthread_mutex_t mutex; // Declaração do mutex
 
-void *usuario_thread(void *arg) {
+void *usuario_thread(void *arg)
+{
     char *diretorio = (char *)arg;
 
-    
-    //Abre o diretório
+    // Abre o diretório
     DIR *dir = opendir(diretorio);
-    if (dir == NULL) {
+    if (dir == NULL)
+    {
         printf("Erro ao abrir o diretorio");
         free(diretorio); // Libera a memória alocada
         pthread_exit(NULL);
     }
 
     struct dirent *entry;
-    //Lê cada entrada do diretório usando a função readdir
-    while ((entry = readdir(dir)) != NULL) {
-        //Se houver a entrada de um arquivo regular
-        if (entry->d_type == DT_REG) {
-            pthread_mutex_lock(&mutex); //Bloqueia o mutex antes de imprimir
-            printf("[%s] -> %s\n", diretorio, entry->d_name);
-            pthread_mutex_unlock(&mutex); //Desbloqueia o mutex após imprimir
+    // Define os nomes de todos os arquivos existentes
+    const char *arquivos[MAX_ARQUIVOS] = {
+        "file1.csv", "file2.csv", "file3.csv", "file4.csv",
+        "file5.csv", "file6.csv", "file7.csv", "file8.csv",
+        "file9.csv", "file10.csv", "file11.csv", "file12.csv"};
+    // Vetor para armazenar os arquivos ausentes
+    char *arq_ausentes[MAX_ARQUIVOS];
+    int num_ausentes = 0;
+    // inicializa o vetor
+    for (int i = 0; i < MAX_ARQUIVOS; i++)
+        arq_ausentes[i] = NULL;
+
+    // Lê cada entrada do diretório usando a função readdir
+    while ((entry = readdir(dir)) != NULL)
+    {
+        // Se houver a entrada de um arquivo regular
+        if (entry->d_type == DT_REG)
+        {
+            pthread_mutex_lock(&mutex); // Bloqueia o mutex antes de imprimir
+            printf("[%s] Arquivos Existentes -> %s\n", diretorio, entry->d_name);
+            pthread_mutex_unlock(&mutex); // Desbloqueia o mutex após imprimir
+
+            // Compara arquivo com a lista de todos os arquivos
+            for (int i = 0; i < MAX_ARQUIVOS; i++)
+            {
+                if (strcmp(entry->d_name, arquivos[i]) == 0)
+                {
+
+                    break;
+                }
+                else
+                {
+                    arq_ausentes[i] = entry->d_name;
+                    num_ausentes++;
+                }
+            }
         }
     }
-    //Diretorio fechado
+
+    // Teste Ausentes
+    pthread_mutex_lock(&mutex); // Bloqueia o mutex antes de imprimir
+
+    for (int i = 0; i < MAX_ARQUIVOS; i++)
+    {
+        printf("[%s] Arquivos Ausentes -> %s\n", diretorio, arq_ausentes[i]);
+    }
+    printf("\n");
+
+    pthread_mutex_unlock(&mutex); // Desbloqueia o mutex após imprimir
+
+    // Diretorio fechado
     closedir(dir);
     free(diretorio);
     pthread_exit(NULL);
     return NULL;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 4) {
+int main(int argc, char *argv[])
+{
+    if (argc < 4)
+    {
         printf("Informe a quantidade de usuarios, quantidade de arquivos, tamanho do fragmento de arquivo e tamanho do buffer como argumento!\n");
         return 1;
     }
@@ -51,51 +96,36 @@ int main(int argc, char *argv[]) {
     int max_arquivos = atoi(argv[2]);
     /*int tmn_Fragmento = atoi(argv[3]);
     int tmn_Buffer = atoi(argv[4]);*/
-    //Define os nomes de todos os arquivos existentes
-    const char* arquivos[MAX_ARQUIVOS] = {
-        "file1.csv", "file2.csv", "file3.csv", "file4.csv",
-        "file5.csv", "file6.csv", "file7.csv", "file8.csv",
-        "file9.csv", "file10.csv", "file11.csv", "file12.csv"
-    }; 
 
-    if (usuarios <= 0 || usuarios > MAX_USUARIOS) {
+    if (usuarios <= 0 || usuarios > MAX_USUARIOS)
+    {
         printf("Quantidade de usuários invalida!\n");
         return 1;
     }
-    if (max_arquivos <= 0 || max_arquivos > MAX_ARQUIVOS) {
+    if (max_arquivos <= 0 || max_arquivos > MAX_ARQUIVOS)
+    {
         printf("Quantidade de arquivos invalida!\n");
         return 1;
     }
 
-
-
-
     pthread_t threads[MAX_USUARIOS];
     char diretorio[4];
-    pthread_mutex_init(&mutex, NULL); //Mutex iniciado
-
+    pthread_mutex_init(&mutex, NULL); // Mutex iniciado
     // Criação das threads dos usuários
-    for (int i = 0; i < usuarios; i++) {
-        sprintf(diretorio, "U%d", i+1);
-        char *diretorio_thread = malloc(strlen(diretorio) + 1); //Aloca memória para o nome do diretório
+    for (int i = 0; i < usuarios; i++)
+    {
+        sprintf(diretorio, "U%d", i + 1);
+        char *diretorio_thread = malloc(strlen(diretorio) + 1); // Aloca memória para o nome do diretório
         strcpy(diretorio_thread, diretorio);
         pthread_create(&threads[i], NULL, usuario_thread, diretorio_thread);
     }
-
     // Aguarda a finalização das threads dos usuários
-    for (int i = 0; i < usuarios; i++) {
+    for (int i = 0; i < usuarios; i++)
+    {
         pthread_join(threads[i], NULL);
     }
 
-
-
-
-
-
-
-
-
-    pthread_mutex_destroy(&mutex); //Destroi o mutex
+    pthread_mutex_destroy(&mutex); // Destroi o mutex
 
     return 0;
 }
