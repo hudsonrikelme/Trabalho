e090f1d203244bc4afa246173903b5886216e51f
hudsonrikelme <hudsonrikelme@hotmail.com>
2023-06-16 19:14:28 -0300

    Resolução de alguns problemas

--- main.c
+++ main.c
@@ -16,11 +16,33 @@ pthread_mutex_t mutex; // Declaração do mutex
 
 typedef struct
 {
+    char nome[MAX_NOME_ARQUIVO];
+    // int fragmentos;
+} Arquivo;
+
+typedef struct
+{
     char nome[10];
-    char arquivos[MAX_ARQUIVOS][100];
-    char ausentes[MAX_ARQUIVOS][100];
+    Arquivo arquivos[MAX_ARQUIVOS];
+    char ausentes[MAX_ARQUIVOS][MAX_NOME_ARQUIVO];
     int num_arquivos;
 } Usuario;
+
+typedef struct
+{
+    Arquivo arquivo;
+    Usuario *usuario;
+    struct solicitacoes *proximo;
+} Solicitacao;
+
+typedef struct{
+    Usuario *usuario;
+    int tmn_fragmento;
+    int tmn_buffer;
+}ThreadArgs;
+
+
+Solicitacao solicitacoes;
 
 Usuario usuarios[MAX_USUARIOS];
 
@@ -41,14 +63,25 @@ void *receber_arquivo_thread(void *arg)
     return NULL;
 }
 
+
+/**
+ * 
+ * Thread de Usuario
+ * 
+*/
 void *usuario_thread(void *arg)
 {
-    Usuario *usuario = (Usuario *)arg;
+    ThreadArgs *args = (ThreadArgs *)arg;
+    Usuario *usuario = args->usuario;
+    //int tmn_fragmento = args->tmn_fragmento;
+    //int tmn_buffer = args->tmn_buffer;
+
     char *diretorio = usuario->nome;
-    //pthread_t threads_arquivos[MAX_ARQUIVOS];
-    //  Abre o diretório
+    // pthread_t threads_arquivos[MAX_ARQUIVOS];
+    //   Abre o diretório
 
     DIR *dir = opendir(diretorio);
+
     if (dir == NULL)
     {
         printf("Erro ao abrir o diretorio");
@@ -63,6 +96,7 @@ void *usuario_thread(void *arg)
         "file5.csv", "file6.csv", "file7.csv", "file8.csv",
         "file9.csv", "file10.csv", "file11.csv", "file12.csv"};
 
+
     // Lê cada entrada do diretório usando a função readdir
     while ((entry = readdir(dir)) != NULL)
     {
@@ -72,7 +106,7 @@ void *usuario_thread(void *arg)
             pthread_mutex_lock(&mutex); // Bloqueia o mutex antes de imprimir
             // Teste obs. Excluir depois
             printf("[%s] Arquivos Existentes -> %s\n", diretorio, entry->d_name);
-            strcpy(usuario->arquivos[usuario->num_arquivos], entry->d_name);
+            strcpy(usuario->arquivos[usuario->num_arquivos].nome, entry->d_name);
             usuario->num_arquivos++;
             pthread_mutex_unlock(&mutex); // Desbloqueia o mutex após imprimir
         }
@@ -84,7 +118,7 @@ void *usuario_thread(void *arg)
         bool ausente = true;
         for (int j = 0; j < usuario->num_arquivos; j++)
         {
-            if (strcmp(usuario->arquivos[j], arquivos_desejados[i]) == 0)
+            if (strcmp(usuario->arquivos[j].nome, arquivos_desejados[i]) == 0)
             {
                 ausente = false;
                 break;
@@ -100,6 +134,7 @@ void *usuario_thread(void *arg)
             pthread_mutex_unlock(&mutex);
         }
     }
+
 
     pthread_t threads_arquivos[MAX_ARQUIVOS];
     // Analisa quais arquivos o usuário não tem e cria as threads de solicitação de arquivos
@@ -133,19 +168,15 @@ void *usuario_thread(void *arg)
     return NULL;
 }
 
-int main(int argc, char *argv[])
+
+
+/**
+ * 
+ * Avalia quantidade de cada argumento de acordo com os limites pré-estabelecidos
+ * 
+*/
+int avaliacao_requisitos(int num_usuarios, int max_arquivos, int tmn_fragmento, int tmn_buffer)
 {
-    if (argc < 4)
-    {
-        printf("Informe a quantidade de usuarios, quantidade de arquivos, tamanho do fragmento de arquivo e tamanho do buffer como argumento!\n");
-        return 1;
-    }
-
-    int num_usuarios = atoi(argv[1]);
-    int max_arquivos = atoi(argv[2]);
-    /*int tmn_Fragmento = atoi(argv[3]);
-    int tmn_Buffer = atoi(argv[4]);*/
-
     if (num_usuarios <= 0 || num_usuarios > MAX_USUARIOS)
     {
         printf("Quantidade de usuários invalida!\n");
@@ -153,14 +184,43 @@ int main(int argc, char *argv[])
     }
     if (max_arquivos <= 0 || max_arquivos > MAX_ARQUIVOS)
     {
-        printf("Quantidade de arquivos invalida!\n");
+        printf("Quantidade máxima de arquivos invalida!\n");
+        return 1;
+    }
+    if (tmn_fragmento <= 0)
+    {
+        printf("tamanho dos fragmentos invalido!\n");
+        return 1;
+    }
+    if (tmn_buffer <= 0)
+    {
+        printf("Tamanho do buffer invalido!\n");
+        return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 4)
+    {
+        printf("Quantidade de argumentos incorreta!\n");
+        return 1;
+    }
+    int num_usuarios = atoi(argv[1]);
+    int max_arquivos = atoi(argv[2]);
+    int tmn_fragmento = atoi(argv[3]);
+    int tmn_buffer = atoi(argv[4]);
+
+    if(avaliacao_requisitos(num_usuarios, max_arquivos, tmn_fragmento, tmn_buffer) != 0){
         return 1;
     }
 
     srand(time(0)); // Inicializa a semente de números aleatórios
-
     pthread_t threads[num_usuarios];
     Usuario usuarios[num_usuarios];
+    ThreadArgs threadArgs[num_usuarios];
+
 
     pthread_mutex_init(&mutex, NULL); // Mutex iniciado
     // Criação das threads dos usuários
@@ -168,7 +228,12 @@ int main(int argc, char *argv[])
     {
         sprintf(usuarios[i].nome, "U%d", i + 1);
         usuarios[i].num_arquivos = 0;
-        pthread_create(&threads[i], NULL, usuario_thread, &usuarios[i]);
+
+        threadArgs[i].usuario = &usuarios[i];
+        threadArgs[i].tmn_fragmento = tmn_fragmento;
+        threadArgs[i].tmn_buffer = tmn_buffer;
+
+        pthread_create(&threads[i], NULL, usuario_thread, &threadArgs[i]);
     }
     // Aguarda a finalização das threads dos usuários
     for (int i = 0; i < num_usuarios; i++)
